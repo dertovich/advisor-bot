aa5edc35e4a42275ddd0690aa64ec16186d7dbef
dertovich <huni_19@mail.ru>
2023-04-28 01:39:07 +0300

    token receipt

---
+++ go.mod
@@ -0,0 +1,3 @@
+module advisor-bot
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+func mustToken() string {
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not speciefied")
+	}
+
+	return *token
+}
+
+func main() {
+	token := mustToken()
+
+	// tgClient = telegram.New(token)
+
+	// fetcher = fetcher.New()
+
+	// processor = processor.New()
+
+	// consumer.Start(fetcher, processor)
+}
